886472ff3d7ebd7c3adc3101b613caefdfff02a1
PHANAGUS <phanarin03072002@gmail.com>
2022-01-11 16:27:58 +0700

    lab15_2.cpp completed

--- lab15_2.cpp
+++ lab15_2.cpp
@@ -19,3 +19,49 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+
+void inputMatrix(double M[][N])
+{
+	for(int i=0 ; i<N ; i++)
+	{
+		cout << "Row " << i+1 << ": ";
+		for(int j=0 ; j<N ; j++)
+		{
+			cin >> M[i][j];
+		}
+	}
+}
+
+void findLocalMax(const double M[][N], bool Lmx[][N])
+{
+	for(int i=0 ; i<N ; i++)
+	{
+		for(int j=0 ; j<N ; j++)
+		{
+			Lmx[i][j] = 0;
+		}
+	}
+
+	for(int i=1 ; i<N-1 ; i++)
+	{
+		for(int j=1 ; j<N-1 ; j++)
+		{
+			if(M[i][j] >= M[i-1][j] && M[i][j] >= M[i+1][j] && M[i][j] >= M[i][j-1] && M[i][j] >= M[i][j+1])
+			{
+				Lmx[i][j] = 1;
+			}
+		}
+	}
+}
+
+void showMatrix(const bool Lmx[][N])
+{
+	for(int i=0 ; i<N ; i++)
+	{
+		for(int j=0 ; j<N ; j++)
+		{
+			cout << Lmx[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
